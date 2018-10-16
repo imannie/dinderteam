0426d27fa3ff1cd702773dd0b22fd853fc4ab408
Simon Tekeste <st.tekeste@gmail.com>
2018-10-15 20:46:03 -0700

    for loop used to pick specific field

--- Api_control/calling_requests.py
+++ Api_control/calling_requests.py
@@ -1,7 +1,12 @@
 import requests
+import json
 from header_for_request import header
 
-response = requests.get("https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972", headers=header)
-data = response.json()
-print(data)
+#header has the authorization for the api
+response = requests.get("https://api.yelp.com/v3/businesses/search?term=food&location=oakland", headers=header)
+data = response.json() #turns the api response into a json
 
+
+#this allows us to pick out specific things
+for x in data['businesses']:
+    print(x['image_url'],"\n")
