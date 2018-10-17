92886b3b3dea3229d7ec241e56034a333caff214
Simon Tekeste <st.tekeste@gmail.com>
2018-10-17 01:06:37 -0700

    Images being displayed, api working!

--- Api_control/calling_requests.py
+++ Api_control/calling_requests.py
@@ -7,6 +7,7 @@ response = requests.get("https://api.yelp.com/v3/businesses/search?term=food&loc
 data = response.json() #turns the api response into a json
 
 
+
 #this allows us to pick out specific things
 for x in data['businesses']:
     print(x['image_url'],"\n")
--- webfolder/dinder/settings.py
+++ webfolder/dinder/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'restaurants'
+    'restaurants',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "new_age")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
