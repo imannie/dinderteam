70b9dfcdb3f20da67ce2011e7cf9590aa9e1da0c
Jordan Sloop <jsloop310@gmail.com>
2018-10-17 17:36:07 -0700

    more dns store

--- Api_control/calling_requests.py
+++ Api_control/calling_requests.py
@@ -1,12 +1,15 @@
 import requests
 import json
 from header_for_request import header
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 #header has the authorization for the api
 response = requests.get("https://api.yelp.com/v3/businesses/search?term=food&location=oakland", headers=header)
 data = response.json() #turns the api response into a json
+pp.pprint(data)
 
-
-#this allows us to pick out specific things
-for x in data['businesses']:
-    print(x['image_url'],"\n")
+# #this allows us to pick out specific things
+# for x in data['businesses']:
+#     print(x['image_url'],"\n")
