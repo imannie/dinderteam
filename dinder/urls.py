cd3cff0da77fe7747f0f1074c09acfd3870d51eb
Jordan Sloop <jsloop310@gmail.com>
2018-10-19 16:56:41 -0700

    working on homepage forms

--- dinder/urls.py
+++ dinder/urls.py
@@ -5,5 +5,6 @@ from restaurants import views
 
 urlpatterns = [
     
-    path('',views.index),
+    path('',views.homepage),
+    path("index/", views.index)
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 import requests
 import sqlite3
+from django import forms
+
+
 
 header = {
     "Authorization":  "Bearer EgNHeojg_ryrKUYzlgCaPMXU7i60GOR-Yy1qxnoYvIDNM8OEq1bfq1a5cbuiExw94-oDF86cKIGfZI73iQoXsxZYndshHdSCeqUMjCi1C-KqdY1jA2Rkw5O4OQWwWnYx"
@@ -8,23 +11,54 @@ header = {
 response = requests.get("https://api.yelp.com/v3/businesses/search?term=food&location=oakland", headers=header)
 data = response.json()
 
+names = []
 for info in data['businesses']:
     name = info["alias"]
     price = info["price"]
     rating = info["rating"]
     image = info["image_url"]
+    names.append(name)
+
+print(names)
+# dropdown_choices= [
+#     (name, name)
+
+# ]
+
+
+# class FiltersForm(forms.Form):
+#     # city = forms.CharField(label="Food Type", widget=forms.Select(choices=dropdown_choices[1]))
+#     # price = forms.CharField(label="Food Type", widget=forms.Select(choices=dropdown_choices[1]))
+#     # rating = forms.CharField(label="Food Type", widget=forms.Select(choices=dropdown_choices[2]))
+#     food_type = forms.CharField(label="Food Type", widget=forms.Select(choices=names))
+   
+
+
+
+def homepage(request):
+    form  = FiltersForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            return redirect("/index/")
+    context = {
+        'form': form,
+     }
+
+    return render(request, "../new_age/homepage.html", context)
+
+
 def index(request):
 
-    # if request.method == "POST":
+    if request.method == "POST":
 
-    #     user = User.objects.create(
-    #         name=name,
-    #         price=price,
-    #         rating=last_name,
-    #         image=password,
-    #         )
+        user = User.objects.create(
+            name=name,
+            price=price,
+            rating=last_name,
+            image=password,
+            )
 
-    #     user.save()
+        user.save()
 
     if request.method == "post":
         hold = 1
