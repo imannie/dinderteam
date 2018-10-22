b9c1fdda35a2ae0a61a7db6382cb1bd8967b89f6
Rudy Trubitt <rtrubitt@lionel.com>
2018-10-22 15:50:15 -0700

    merged small differences in urls.py

--- dinder/urls.py
+++ dinder/urls.py
@@ -7,9 +7,5 @@ urlpatterns = [
     
     path('',views.index),
     path('index',views.index),
-<<<<<<< HEAD
-    # path('winner',views.winner),
-=======
     path('swipe',views.swipe),
->>>>>>> 43434ed75b27e051af56d1c2d9272a9f05f0f0f4
 ]
