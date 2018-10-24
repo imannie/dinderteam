9bbde46017e40d16615b20565c0c4ea8aefee23a
Simon Tekeste <st.tekeste@gmail.com>
2018-10-23 21:06:12 -0700

    updates the db and pulls at random based on the session_key

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Restaurants_info(models.Model):
     name = models.CharField(max_length=300)
@@ -8,3 +9,7 @@ class Restaurants_info(models.Model):
     image = models.CharField(max_length=300)
     hold = models.CharField(max_length=300)
     selected = models.CharField(max_length=300,default="")
+    url = models.CharField(max_length=1000,default="") 
+    phone = models.CharField(max_length=300,default="")
+    address = models.CharField(max_length=500, default="")
+    session_key = models.CharField(max_length=300,null=True)
