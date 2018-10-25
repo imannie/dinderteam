64b13bbe17e68f3cbbb8c2be1bf36c6e3b21bacb
Simon Tekeste <st.tekeste@gmail.com>
2018-10-24 17:11:22 -0700

    heroku with db

--- dinder/settings.py
+++ dinder/settings.py
@@ -72,8 +72,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'dinder.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
